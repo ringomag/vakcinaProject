8d25f1e98996674ffa428258c96aac7a5260300f
ringomag <optimuskrajm@gmail.com>
2021-03-11 11:36:15 +0100

    with green vaccine field

--- vakcinisani/models.py
+++ vakcinisani/models.py
@@ -4,11 +4,11 @@ class Vakcinisan(models.Model):
     ime = models.CharField(max_length=200)
     prezime = models.CharField(max_length=200)
     vrste_vakcina = (
-        ("fajzer","fajzer"),
-        ("sputnjk","sputnjik"),
-        ("astra zeneka","astra zeneka"),
-        ("sinofarm","sinofarm"),
-        ("moderna","moderna"),
+        ("Fajzer","Fajzer"),
+        ("Sputnjik","Sputnjik"),
+        ("Astra Zeneka","Astra Zeneka"),
+        ("Sinofarm","Sinofarm"),
+        ("Moderna","Moderna"),
     )
 
     vakcina = models.CharField(max_length=100, choices=vrste_vakcina)
