8f01e2e3b2c0e65cb1fc515e21963d624c4ebc5a
ringomag <optimuskrajm@gmail.com>
2021-03-13 20:25:31 +0100

    upgraded home page, and more

--- vakcinisani/urls.py
+++ vakcinisani/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index ,name='index'),
-    path('delete/<int:id>', views.delete, name='delete')
+    #path('', views.index ,name='index'),
+    path('', views.home, name='home'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('lista/', views.lista, name='lista'),
+    path('novi_pacijent/', views.novi, name='novi'),
 ]
--- vakcinisani/views.py
+++ vakcinisani/views.py
@@ -5,6 +5,9 @@ from django.http import HttpResponseRedirect, HttpResponse
 
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html', {})
+
 def index(request):
     kreirani = Vakcinisan.objects.all()
     form = VakcinisanForm
@@ -20,9 +23,23 @@ def index(request):
     context = {'form':form, 'kreirani':kreirani}
     return render(request, 'index.html', context)
 
-
-
-
 def delete(request, id):
     Vakcinisan.objects.get(id=id).delete()
     return HttpResponseRedirect('/')
+
+
+
+# lista građana koji su izrazili zaintersovanost za vakcinu
+def lista(request):
+    kreirani = Vakcinisan.objects.all()
+    return render(request, 'lista.html', {'kreirani':kreirani})
+
+# ova funkcija je za novog gradjanina koji se prijavljuje za vakcinu
+def novi(request):
+    form = VakcinisanForm
+    if request.method == 'POST':
+        form = VakcinisanForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/lista/')
+    return render(request, 'novi_pacijent.html', {'form':form})
